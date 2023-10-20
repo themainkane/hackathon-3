38122a1e5561c50da61af9f7d32780d6df291477
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-20 12:31:30 +0200

    checked

--- database/migrations/2023_10_20_085235_create_owners_table.php
+++ database/migrations/2023_10_20_085235_create_owners_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -16,7 +17,7 @@ return new class extends Migration {
             $table->string('surname')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
-            $table->string('address')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
